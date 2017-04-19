049bf73dfcb59f53b33470d57e3c66992996113d
amandacirverius <amanda.cirverius@gmail.com>
2017-04-19 13:55:54 +0200

    User

--- UserPages/src/main/java/se/nackademin/domain/UserHandler.java
+++ UserPages/src/main/java/se/nackademin/domain/UserHandler.java
@@ -1,24 +1,19 @@
 package se.nackademin.domain;
 
+import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named
 @SessionScoped
-public class UserHandler {
+public class UserHandler implements Serializable {
     
     public void doRegister(User selectedUser){
         //TODO; Lägga till en varning om id redan finns
-        if(!userExists(selectedUser)){
+        if(!FakeDB.isUserExists(selectedUser)){
           FakeDB.addUser(selectedUser);
         }
     }
     
-    private boolean userExists(User selectedUser){
-        User dbUser = FakeDB.getUser(selectedUser.getUsername());
-        
-    }
-    
-    
-    
+
 }
